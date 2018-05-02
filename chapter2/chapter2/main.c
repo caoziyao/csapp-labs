116adaa310f4840303b95f2c384dc115f061b50b
cczy <wyzycao@gmail.com>
2018-05-02 20:17:50 +0800

    chapter2

---
+++ chapter2/chapter2/main.c
@@ -0,0 +1,69 @@
+//
+//  main2.c
+//  chapter2
+//
+//  Created by working on 2018/5/2.
+//  Copyright © 2018年 working. All rights reserved.
+//
+
+//
+//  main.c
+//  chapter2
+//
+//  Created by working on 2018/5/2.
+//  Copyright © 2018年 working. All rights reserved.
+//
+
+#if 0
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void
+show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++) {
+        printf("%.2x ", start[i]);
+    }
+    printf("\n");
+}
+
+void
+show_int(int x) {
+    show_bytes((byte_pointer)&x, sizeof(int));
+}
+
+void
+show_float(float x) {
+    show_bytes((byte_pointer)&x, sizeof(float));
+}
+
+void
+show_pointer(void *x) {
+    show_bytes((byte_pointer)&x, sizeof(void *));
+}
+
+/*
+ 39 30 00 00
+ 00 e4 40 46
+ f8 f5 bf ef fe 7f 00 00
+ */
+void
+test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float)ival;
+    int *pval = &ival;
+    
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int val = 12345;
+    test_show_bytes(val);
+    return 0;
+}
+
+#endif
