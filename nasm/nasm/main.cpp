6f87eb38fe23d4c8509dcf6d17cb9c4e24ea21ae
cczy <wyzycao@gmail.com>
2018-09-25 02:14:27 +0800

    nasm debug

---
+++ nasm/nasm/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  nasm
+//
+//  Created by working on 2018/9/25.
+//  Copyright © 2018年 working. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+extern "C" long long GetRDTSC();
+
+int main(int, const char**)
+{
+    long long RDTSC1 = GetRDTSC();
+    long long RDTSC2 = GetRDTSC();
+    printf("Time-Stamp Counters: %lld - %lld\n", RDTSC1, RDTSC2);
+    return 0;
+}
